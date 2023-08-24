3d7ca3adb2cf1244c59ad54ba435f8dbd1aeb3fb
Dharmik Gadhiya <dharmikgadhiya52525@gmail.com>
2023-08-24 09:13:37 +0530

    modal

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Header from './pages/common/header'
 import Footer from "./pages/common/footer"
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { useState } from 'react';
+import * as XLSX from 'xlsx'; // Library to parse Excel files
+
+import { Modal, Button } from 'react-bootstrap';
+
 function App() {
   const [data, setData] = useState([
     {
@@ -38,12 +42,23 @@ function App() {
     },
     // Add more data entries as needed
   ]);
+  const [show, setShow] = useState(true)
   const [editing, setEditing] = useState(false);
+  const [file, setFile] = useState(null);
+  const [textInput, setTextInput] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   const categories = ['Category 1', 'Category 2', 'Category 3'];
   const colors = ['Red', 'Green', 'Blue'];
   const sizes = ['Small', 'Medium', 'Large'];
 
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
   const handleCheckboxChange = (id) => {
     setData((prevData) =>
       prevData.map((item) =>
@@ -68,8 +83,68 @@ function App() {
     );
   };
 
+  const handleTextInputChange = (event) => {
+    setTextInput(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Handle form submission here
+    // You can use 'file' and 'textInput' state values
+    // Reset state values if needed
+    closeModal();
+  };
+
+
+  //file 
+  const [excelData, setExcelData] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+
+        const firstSheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[firstSheetName];
+
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+        if (jsonData.length > 1) {
+          const headers = jsonData[0];
+          const rows = jsonData.slice(1);
+
+          const transformedData = rows.map((row) => {
+            const rowData = {};
+            headers.forEach((header, index) => {
+              rowData[header] = row[index];
+            });
+            return rowData;
+          });
+
+          setExcelData(transformedData);
+        } else {
+          setExcelData([]);
+        }
+      };
+
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
+
+  const printExcelData = () => {
+    console.log(excelData);
+  };
+
   return (
     <div>
+      <Button variant="primary" onClick={openModal}>
+        Open Modal
+      </Button>
       <table>
         <thead>
           <tr>
@@ -177,11 +252,27 @@ function App() {
           ))}
         </tbody>
       </table>
+      <Modal show={showModal} handleClose={closeModal} >
+        <Modal.Header closeButton>
+          <Modal.Title>My Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input type="file" accept=".xlsx" onChange={handleFileChange} />
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+          <button onClick={printExcelData}>Print Excel Data</button>
+
+        </Modal.Footer>
+      </Modal>
       <button onClick={handleDeleteSelected}>Delete Selected</button>
       <button onClick={handleEditToggle}>
         {editing ? 'Save Changes' : 'Edit'}
       </button>
-    </div>);
+    </div >);
 }
 
 export default App;
